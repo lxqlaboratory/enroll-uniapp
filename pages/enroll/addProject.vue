f524935fa5690fb60bd475bcb434cea4bd25556d
wenxue11 <714876076@qq.com>
2020-03-22 20:07:46 +0800

    wx

--- pages/enroll/addProject.vue
+++ pages/enroll/addProject.vue
@@ -159,8 +159,8 @@
 	<view>
 		<uni-section title="报名项" type="line"></uni-section>
 		<uni-swipe-action>
-			<uni-swipe-action-item v-for="(item,index) in ProjectInstanceList" :options="options2" :key="item.instanceId"  @click="swipeClick($event,index,item.instanceId)">
-				<text class="cont">{{item.instanceNum}}-{{item.instanceName}}-{{item.year}}年</text>
+			<uni-swipe-action-item v-for="(item,index) in itemList" :options="options2" :key="item.itemId"  @click="swipeClick($event,index,item.itemId)">
+				<text class="cont">{{item.itemName}}-{{item.needCount}}人</text>
 			</uni-swipe-action-item>
 		</uni-swipe-action>
 		<button class="button-cell" @click="addItem">添加报名项</button>
@@ -212,6 +212,17 @@
 					viewStartDate: '',
 					viewStartTime: ''
 				},
+				options2: [{
+					text: '编辑',
+					style: {
+						backgroundColor: '#7acfa6'
+					}
+				}, {
+					text: '删除',
+					style: {
+						backgroundColor: 'rgb(255,58,49)'
+					}
+				}],
 				projectId: '',
 				date: currentDate,
 				showyemian: false,
@@ -308,11 +319,185 @@
 			            month = month > 9 ? month : '0' + month;;
 			            day = day > 9 ? day : '0' + day;
 			            return `${year}-${month}-${day}`;
-			        }
+			        },
+					
+					
+					
+					
+					
+					
+					swipeClick(e, index,instanceId) {
+						let {
+							content
+						} = e
+						if (content.text === '删除') {
+							uni.showModal({
+								title: '提示',
+								content: '是否删除',
+								success: (res) => {
+									if (res.confirm) {
+										
+										enrollItemPersonApplyCancel({itemPersonId:id}).then(res => {
+														 if(res.re === 1){
+															 this.personList.splice(index,1);
+															 uni.showToast({
+															 	title: `删除成功`,
+															 	icon: 'none'
+															 })
+														 }
+													
+													}).catch(err => {
+														
+													})
+									} else  {
+										uni.showToast({
+											title: `已取消操作`,
+											icon: 'none'
+										})
+									}
+								}
+							});
+						} else {
+							uni.navigateTo({
+								url:'./addProject?instanceId='+instanceId+'&&projectId='+this.projectId+''
+							})
+							
+						}
+								
+					}
+								
 		}
 	}
 </script>
 
 <style>
+	.cont {
+		flex: 1;
+		height: 45px;
+		line-height: 45px;
+		padding: 0 15px;
+		position: relative;
+		background-color: #fff;
+		font-size: 15px;
+		border-bottom-color: #F5F5F5;
+		border-bottom-width: 1px;
+		border-bottom-style: solid;
+	}
 	
+	/* 头条小程序组件内不能引入字体 */
+	/* #ifdef MP-TOUTIAO */
+	@font-face {
+		font-family: uniicons;
+		font-weight: normal;
+		font-style: normal;
+		src: url('~@/static/uni.ttf') format('truetype');
+	}
+	
+	/* #endif */
+	
+	/* #ifndef APP-NVUE */
+	page {
+		display: flex;
+		flex-direction: column;
+		box-sizing: border-box;
+		background-color: #efeff4;
+		min-height: 100%;
+		height: auto;
+	}
+	
+	view {
+		font-size: 14px;
+		line-height: inherit;
+	}
+	
+	.example {
+		padding: 0 15px 15px;
+	}
+	
+	.example-info {
+		padding: 15px;
+		color: #3b4144;
+		background: #ffffff;
+	}
+	
+	.example-body {
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		padding: 0;
+		font-size: 14px;
+		background-color: #ffffff;
+	}
+	
+	/* #endif */
+	.example {
+		padding: 0 15px;
+	}
+	
+	.example-info {
+		/* #ifndef APP-NVUE */
+		display: block;
+		/* #endif */
+		padding: 15px;
+		color: #3b4144;
+		background-color: #ffffff;
+		font-size: 14px;
+		line-height: 20px;
+	}
+	
+	.example-info-text {
+		font-size: 14px;
+		line-height: 20px;
+		color: #3b4144;
+	}
+	
+	
+	.example-body {
+		flex-direction: column;
+		padding: 15px;
+		background-color: #ffffff;
+	}
+	
+	.word-btn-white {
+		font-size: 18px;
+		color: #FFFFFF;
+	}
+	
+	.word-btn {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		border-radius: 6px;
+		height: 48px;
+		margin: 15px;
+		background-color: #007AFF;
+	}
+	
+	.word-btn--hover {
+		background-color: #4ca2ff;
+	}
+	
+	
+	
+	.search-result {
+		margin-top: 10px;
+		margin-bottom: 20px;
+		text-align: center;
+	}
+	
+	.search-result-text {
+		text-align: center;
+		font-size: 14px;
+	}
+	
+	.example-body {
+		/* #ifndef APP-NVUE */
+		display: block;
+		/* #endif */
+		padding: 0px;
+	}
 </style>
+
